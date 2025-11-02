97601d381a2894b13e9d5a2a4baeda65df6cbe2a
Ruz <ruzzxv@gmail.com>
2025-11-03 06:50:50 +1100

    Configure Astro for custom domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://RuzXV.github.io', 
+  site: 'https://codexhelper.com', 
 
-  base: '/codexhelper-astro',
+  base: '/',
 });
